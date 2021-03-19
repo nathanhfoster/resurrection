7f41673e4207d4cd9cc9831495af765a6f172901
Nathan Foster <nathan.foster@passportinc.com>
2021-03-19 15:56:36 -0700

    U

--- src/utils/defaultReducer.js
+++ src/utils/defaultReducer.js
@@ -2,7 +2,7 @@ import getDerivedStateFromProps from './getDerivedStateFromProps'
 /**
  * Mimics React.Component this.setState
  * @param {ReducerState} prevState - previous reducer state
- * @param {Object} action - the state keys to overwrite
+ * @param {ReducerState} action - the state keys to overwrite
  * @returns {ReducerState} - the next state for the reducer
  */
 const defaultReducer = (prevState, action) => getDerivedStateFromProps(prevState, action)
