0f28b5347ecaf64842555f221066131e5af89458
Nathan Foster <nateinthegame@gmail.com>
2021-05-25 20:35:55 -0700

    Allow callback

--- src/utils/defaultReducer.js
+++ src/utils/defaultReducer.js
@@ -1,10 +1,14 @@
 import getDerivedStateFromProps from './getDerivedStateFromProps';
+import isFunction from './isFunction';
 /**
  * Mimics React.Component this.setState
  * @param {ReducerState} state - current reducer state
- * @param {ReducerState} action - the state keys to overwrite
+ * @param {ReducerState|Function} action - the state keys to overwrite
  * @returns {ReducerState} - the next state for the reducer
  */
-const defaultReducer = (state, action) => getDerivedStateFromProps(state, action);
+const defaultReducer = (state, action) => {
+   const nextState = isFunction(action) ? action(state) : action;
+   return getDerivedStateFromProps(state, nextState)
+};
 
 export default defaultReducer;
