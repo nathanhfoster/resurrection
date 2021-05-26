910f3a910a9c93411c8b10988b8e729658b25b9d
Nathan Foster <nateinthegame@gmail.com>
2021-05-26 11:17:38 -0700

    Update defaultReducer.js

--- src/utils/defaultReducer.js
+++ src/utils/defaultReducer.js
@@ -4,12 +4,19 @@ import isFunction from './isFunction';
  * Mimics React.Component this.setState
  * @param {ReducerState} state - current reducer state
  * @param {ReducerState|function(): ReducerState} action - the state keys to overwrite
+ * @param {Function} callback - mimics the second parameter of this.setState
  * @returns {ReducerState} - the next state for the reducer
  */
 
-const defaultReducer = (state, action) => {
-  const nextState = isFunction(action) ? action(state) : action;
-  return getDerivedStateFromProps(state, nextState);
+const defaultReducer = (state, action, callback) => {
+  const nextStateToOverwrite = isFunction(action) ? action(state) : action;
+  const nextState = getDerivedStateFromProps(state, nextState);
+ 
+  if(isFunction(callback)) {
+    callback(nextState);
+  };
+
+   return nextState;
 };
 
 export default defaultReducer;
