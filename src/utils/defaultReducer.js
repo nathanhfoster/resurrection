8c251b5f7ab17b7833038601a206a99c2257e3f6
Nate <nathan@ecocart.io>
2021-05-25 20:40:26 -0700

    Documentation

--- src/utils/defaultReducer.js
+++ src/utils/defaultReducer.js
@@ -3,9 +3,10 @@ import isFunction from './isFunction';
 /**
  * Mimics React.Component this.setState
  * @param {ReducerState} state - current reducer state
- * @param {ReducerState|Function} action - the state keys to overwrite
+ * @param {ReducerState|function(): ReducerState} action - the state keys to overwrite
  * @returns {ReducerState} - the next state for the reducer
  */
+
 const defaultReducer = (state, action) => {
    const nextState = isFunction(action) ? action(state) : action;
    return getDerivedStateFromProps(state, nextState)
