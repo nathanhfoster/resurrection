b66a7cc6a48ee2d3b9aba4566524182ddc7538c5
Nate <nathan@ecocart.io>
2021-05-26 00:08:53 -0700

    Docs

--- src/hooks/useSetStateReducer.js
+++ src/hooks/useSetStateReducer.js
@@ -3,8 +3,8 @@ import { defaultReducer } from '../utils'
 
 /**
  * Mimics React.Component this.state and this.setState
- * @param {ReducerState=} initializerArg - initialState
- * @param {ReducerStateInitializer=} initializer - initilizes the reducer's state
+ * @param {ReducerState=} initializerArg - initial state of the reducer
+ * @param {ReducerStateInitializer=} initializer - callback that initilizes the reducer's state
  * @returns {Array.<ReducerState, function(): ReducerState>} - the new useReducer hook
  */
 const useSetStateReducer = (initializerArg = {}, initializer) =>
