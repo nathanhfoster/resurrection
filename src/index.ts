0f72d0783b0cc4af7b6c8962d65a855cb6e5e725
Nate <nathan@ecocart.io>
2021-07-12 00:52:21 -0700

    Test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import * as Types from './types';
+
 import { ContextConsumer, ContextProvider, MemoizedContextProvider, storeFactory } from './provider';
 
 import connect from './connect';
 
-import { stateConnect, StateProvider } from '@context';
+import { stateConnect, StateProvider } from './context';
 
 import {
   actionTypes,
@@ -13,9 +15,9 @@ import {
   getRandomInt,
   isFunction,
   shallowEquals
-} from '@utils';
+} from './utils';
 
-import { setStateReducer, setObjectStateReducer, toggleBooleanReducer, setNumberReducer } from '@reducers';
+import { setStateReducer, setObjectStateReducer, toggleBooleanReducer, setNumberReducer } from './reducers';
 
 import {
   useDispatch,
@@ -24,20 +26,17 @@ import {
   useReducerWithThunk,
   useSelector,
   useSetStateReducer
-} from '@hooks';
+} from './hooks';
 
 export {
-  // provider
+  Types,
   ContextConsumer,
   ContextProvider,
   MemoizedContextProvider,
   storeFactory,
-  // connect
   connect,
-  // context
   stateConnect,
   StateProvider,
-  // utils
   actionTypes,
   bindActionCreators,
   combineReducers,
@@ -46,12 +45,10 @@ export {
   getRandomInt,
   isFunction,
   shallowEquals,
-  // reducer
   setStateReducer,
   setObjectStateReducer,
   toggleBooleanReducer,
   setNumberReducer,
-  // hooks
   useDispatch,
   useLazyMemo,
   usePreviousValue,
