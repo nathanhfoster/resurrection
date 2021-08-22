2f5b1599b45351f127fc49bc02b5247f1350b5a4
Nathan Foster <nateinthegame@gmail.com>
2021-08-21 22:48:03 -0700

    Fixed imports

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,9 @@
-import {
-  StateContextConsumer,
-  DispatchContextConsumer,
-  ContextProvider,
-  MemoizedContextProvider,
-  storeFactory
-} from '@provider';
-
 import connect from '@connect';
+export { default as connect } from '@connect';
+export * from '@provider';
+export * from '@context';
+export * from '@utils';
+export * from '@reducers';
+export * from '@hooks';
 
-import { stateConnect, StateProvider } from '@context';
-
-import {
-  actionTypes,
-  bindActionCreators,
-  combineReducers,
-  defaultInitializer,
-  getDerivedStateFromProps,
-  getRandomInt,
-  isFunction,
-  shallowEquals
-} from '@utils';
-
-import { setStateReducer, setObjectStateReducer, toggleBooleanReducer, setNumberReducer } from '@reducers';
-
-import {
-  useBooleanReducer,
-  useDispatch,
-  useLazyMemo,
-  useMounted,
-  useNumberReducer,
-  useNumberRef,
-  useOutterClick,
-  usePreviousValue,
-  useReducerWithThunk,
-  useSelector,
-  useSetRefState,
-  useSetStateReducer
-} from '@hooks';
-
-export {
-  // provider
-  StateContextConsumer,
-  DispatchContextConsumer,
-  ContextProvider,
-  MemoizedContextProvider,
-  storeFactory,
-  // connect
-  connect,
-  // context
-  stateConnect,
-  StateProvider,
-  // utils
-  actionTypes,
-  bindActionCreators,
-  combineReducers,
-  defaultInitializer,
-  getDerivedStateFromProps,
-  getRandomInt,
-  isFunction,
-  shallowEquals,
-  // reducer
-  setStateReducer,
-  setObjectStateReducer,
-  toggleBooleanReducer,
-  setNumberReducer,
-  // hooks
-  useBooleanReducer,
-  useDispatch,
-  useLazyMemo,
-  useMounted,
-  useNumberReducer,
-  useNumberRef,
-  useOutterClick,
-  usePreviousValue,
-  useReducerWithThunk,
-  useSelector,
-  useSetRefState,
-  useSetStateReducer
-};
+export default connect;
