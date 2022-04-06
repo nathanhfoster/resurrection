445ce05f8194df91d44ba492c7d242948a069d3c
Nathan Foster <nateinthegame@gmail.com>
2022-04-05 18:17:54 -0700

    support forwardRef

--- src/connect.tsx
+++ src/connect.tsx
@@ -13,52 +13,77 @@ import {
 /**
  * This function simulates Redux's connect API
  */
-const connect: ConnectType = (mapStateToProps, mapDispatchToProps, mergeProps, options) => (Component) => {
-  const {
-    stateContext = StateContextConsumer,
-    dispatchContext = DispatchContextConsumer,
-    pure = true,
-    // TODO:
-    // areStatesEqual = shallowEquals,
-    // areOwnPropsEqual = shallowEquals,
-    // areStatePropsEqual = shallowEquals,
-    areMergedPropsEqual = shallowEquals
-    // forwardRef = false,
-  } = options || {};
+const connect: ConnectType = (mapStateToProps, mapDispatchToProps, mergeProps, options) => {
+  const wrapWithConnect = (WrappedComponent) => {
+    const {
+      stateContext = StateContextConsumer,
+      dispatchContext = DispatchContextConsumer,
+      pure = true,
+      // TODO:
+      // areStatesEqual = shallowEquals,
+      // areOwnPropsEqual = shallowEquals,
+      // areStatePropsEqual = shallowEquals,
+      areMergedPropsEqual = shallowEquals,
+      forwardRef = false,
+    } = options || {};
 
-  const handleMergeProps: MergePropsType = isFunction(mergeProps) ? mergeProps : defaultMergeProps;
+    const handleMergeProps: MergePropsType = isFunction(mergeProps) ? mergeProps : defaultMergeProps;
 
-  // Conditionally memoize Component
-  const PureComponent = pure === true ? memo(Component, areMergedPropsEqual) : Component;
+    // Conditionally memoize WrappedComponent
+    const PureComponent = pure === true ? memo(WrappedComponent, areMergedPropsEqual) : WrappedComponent;
 
-  return (ownProps) => {
-    const state: ReducerStateType = useContext<ReducerStateType>(stateContext);
-    const dispatch: DispatchType = useContext<DispatchType>(dispatchContext);
+    const ConnectFunction = ({ forwardedRef, ...ownProps }) => {
+      const state: ReducerStateType = useContext<ReducerStateType>(stateContext);
+      const dispatch: DispatchType = useContext<DispatchType>(dispatchContext);
 
-    const stateToProps: ComponentPropsType = useMemo(() => {
-      if (isFunction(mapStateToProps)) {
-        return mapStateToProps(state, ownProps);
-      }
-      return {};
-    }, [state, ownProps]);
+      const stateToProps: ComponentPropsType = useMemo(() => {
+        if (isFunction(mapStateToProps)) {
+          return mapStateToProps(state, ownProps);
+        }
+        return {};
+      }, [state, ownProps]);
 
-    const dispatchToProps: ComponentPropsType = useMemo(() => {
-      if (!mapDispatchToProps) {
-        return { dispatch };
-      }
-      if (isFunction(mapDispatchToProps)) {
-        return mapDispatchToProps(dispatch as ThunkActionDispatchType);
-      }
-      return bindActionCreators(mapDispatchToProps, dispatch);
-    }, [dispatch]);
+      const dispatchToProps: ComponentPropsType = useMemo(() => {
+        if (!mapDispatchToProps) {
+          return { dispatch };
+        }
+        if (isFunction(mapDispatchToProps)) {
+          return mapDispatchToProps(dispatch as ThunkActionDispatchType);
+        }
+        return bindActionCreators(mapDispatchToProps, dispatch);
+      }, [dispatch]);
 
-    const mergedProps: ComponentPropsType = useMemo(
-      () => handleMergeProps(stateToProps, dispatchToProps, ownProps),
-      [ownProps, stateToProps, dispatchToProps]
-    );
+      const mergedProps: ComponentPropsType = useMemo(
+        () => handleMergeProps(stateToProps, dispatchToProps, ownProps),
+        [ownProps, stateToProps, dispatchToProps]
+      );
 
-    return <PureComponent {...mergedProps} />;
-  };
+      const renderedWrappedComponent = useMemo(() => {
+        const PureComponent = pure ? memo(WrappedComponent, areMergedPropsEqual) : WrappedComponent;
+
+        return <PureComponent {...mergedProps} ref={forwardedRef} />;
+      }, [forwardedRef, mergedProps]);
+
+      return renderedWrappedComponent;
+    };
+
+    const Connect = pure ? memo(ConnectFunction, areMergedPropsEqual) : ConnectFunction;
+
+    Connect.WrappedComponent = WrappedComponent;
+    Connect.displayName = ConnectFunction.displayName = displayName;
+
+    if (forwardRef) {
+      const ForwaredComponent = reactForwardRef((props, ref) => <Connect {...props} forwardedRef={ref} />);
+
+      ForwaredComponent.displayName = displayName;
+      ForwaredComponent.WrappedComponent = WrappedComponent;
+      return ForwaredComponent;
+    }
+
+    return Connect;
+  }
+
+  return wrapWithConnect
 };
 
 export default connect;
