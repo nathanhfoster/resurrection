4f7836fb584e29e0fffed190111a3c6a5f03f2cd
Nathan Foster <nateinthegame@gmail.com>
2022-04-05 19:57:06 -0700

    Documentation

--- src/hooks/useMemoComponent.ts
+++ src/hooks/useMemoComponent.ts
@@ -3,6 +3,13 @@ import { useMemoComponentType } from 'types';
 import usePreviousValue from './usePreviousValue';
 import { isFunction } from 'utils';
 
+/**
+ * Hook that controls the reference of a component to only update when it's previous and next props differ
+ * @param {JSX.Element} Component
+ * @param {*} props
+ * @param {function} isEqual
+ * @returns {JSX.Element}
+ */
 const useMemoComponent: useLazyMemoType = (Component, props, isEqual) => {
     const previousProps = usePreviousValue(props);
     const ComponentRef = useRef(Component);
