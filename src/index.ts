b7760653ef7b4cca300eb9283f82e656d60cc90e
Nathan Foster <nfoster@ranker.com>
2022-05-28 23:06:59 -0400

    fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import connect from 'connect';
 export * from 'provider';
 export * from 'context';
 export * from 'utils';
 export * from 'reducers';
 export * from 'hooks';
-
-export default connect;
+export { default as connect } from 'connect';
